6d2c1f2143840c1d27dd49ece0a394e7cf276716
LiShuxue <1149926505@qq.com>
2018-12-04 11:51:59 +0800

    init this sub app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to module B, this is also a single app."/>
   </div>
 </template>
 
